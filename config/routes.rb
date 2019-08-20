a54900e6fcbdcd41145bfb79dc5d3c3901650404
amberm <nekosenpai@gmail.com>
2019-08-20 15:41:39 -0600

    migrations and active records created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :order_products
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
